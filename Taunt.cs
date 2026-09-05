984a84618f67957f7d78ee1db34667546ea151e9
bradleyethan220-hue <bradleyethan220@gmail.com>
2026-09-05 12:10:00 +0100

    Add Taunt class with random insults

---
+++ Taunt.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace CursedPopupGame
+{
+    public static class Taunt
+    {
+        private static readonly Random rnd = new Random();
+
+        private static readonly string[] insults =
+        {
+            "YOU ARE AN IDIOT HAHA",
+            "CAN'T EVEN CLICK FAST",
+            "ERROR: SKILL NOT FOUND",
+            "YOU ARE A BUG IN THE SYSTEM",
+            "YOU ARE A FAILURE MODULE",
+            "YOU ARE A CORRUPTED USER",
+            "SYSTEM WARNING: YOU SUCK",
+            "YOU ARE NOT EVEN TRYING",
+            "PATH NOT FOUND: YOUR BRAIN",
+            "YOU ARE A GLITCH IN REALITY"
+        };
+
+        public static string GetRandomInsult()
+        {
+            return insults[rnd.Next(insults.Length)];
+        }
+    }
+}
